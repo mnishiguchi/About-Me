f69ff65ef1155e7837e7c33d10c236124c1dd778
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-11-18 14:38:14 -0500

    Refactor

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,14 @@
 (function() {
 
   // Module declaration.
-  angular.module( "myApp", [] );
+  angular.module( "mnApp", [] );
 
 
   // --------------------------------------------------------------------------- //
   // --------------------------------------------------------------------------- //
 
 
-  angular.module( "myApp" )
+  angular.module( "mnApp" )
   .controller(
     "AppController", function() {
       // Empty.
@@ -20,14 +20,14 @@
   // --------------------------------------------------------------------------- //
 
 
-  angular.module( "myApp" )
+  angular.module( "mnApp" )
   .directive(
-    'simpleTabPanel', function () {
+    'tabPanel', function () {
 
     return {
       restrict: "E",
       scope: {},
-      templateUrl: 'views/simple-tab-panel.html',
+      templateUrl: 'views/tab-panel.html',
 
       controllerAs: "vm",
       controller: ['$scope', function( $scope ) {
@@ -36,8 +36,16 @@
         var props = $scope.props = $scope;  // Alias for $scope
 
         // State
-        // Hold the current position of the tabs, initially 1
-        vm.tab = 1;
+        vm.tab = 0;  // Tab position initially 0;
+        vm.tabNames = ['Home', 'About', 'Tools', 'Hobbies', 'Code', 'Contact'];
+        vm.contentUrls = [
+          "/views/content-1.html",
+          "/views/content-2.html",
+          "/views/content-3.html",
+          "/views/content-4.html",
+          "/views/content-5.html",
+          "/views/content-6.html",
+        ];
 
         // Set the tab position
         vm.setTab = function(tab) {
@@ -52,5 +60,28 @@
       }] // end controller
     }; // end return
   }); // end directive
+
+
+  angular.module( "mnApp" )
+  .directive(
+    'siteHeader', function () {
+
+    return {
+      restrict: "E",
+      templateUrl: '/views/header.html',
+    }; // end return
+  }); // end directive
+
+
+  angular.module( "mnApp" )
+  .directive(
+    'siteFooter', function () {
+
+    return {
+      restrict: "E",
+      templateUrl: '/views/footer.html',
+    }; // end return
+  }); // end directive
+
 
 })();
