b3162a56c50080c61da2df963ea4340cd37bf662
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-12-25 20:09:39 -0500

    Refactor tabPanel.directive

--- project/app/components/tabPanel/tabPanel.directive.js
+++ project/app/components/tabPanel/tabPanel.directive.js
@@ -4,18 +4,19 @@
 (function() {
 
   // Module declaration.
-  var module = angular.module(
-  "tabPanelComponents",
-  []);
+  angular
+    .module( "tabPanelComponents", [ ]);
 
 
   // --------------------------------------------------------------------------- //
   // --------------------------------------------------------------------------- //
 
 
-  module.directive(
-  'tabPanel',
-  function() {
+  angular
+    .module( "tabPanelComponents")
+    .directive( "tabPanel", tabPanelDirective );
+
+  function tabPanelDirective() {
 
     return {
       restrict: "E",
@@ -23,75 +24,87 @@
       templateUrl: 'app/components/tabPanel/tabPanel.template.html',
 
       controllerAs: "vm",
-      controller:
-      [
-        "$scope",
-        "$window",
-        function( $scope, $window ) {
+      controller: tabPanelController,
 
-          var vm    = this;
-          var props = $scope.props = $scope;  // Alias for $scope
-
-          // State
-          vm.tabIndex = 0;  // Tab position initially 0;
-          vm.tabNames = [
-            'About me',
-            'Background',
-            "Projects",
-            'Blog',
-            "Resources",
-            'Contact'
-          ];
-          vm.contentUrls = [
-            "app/components/tabPanel/_about_me.template.html",
-            "app/components/tabPanel/_background.template.html",
-            "app/components/tabPanel/_projects.template.html",
-            "app/components/tabPanel/_blog.template.html",
-            "app/components/tabPanel/_resources.template.html",
-            "app/components/tabPanel/_contact.template.html",
-          ];
-
-          // Expose the public methods.
-          vm.setTab = setTab;
-          vm.isSet  = isSet;
-
-
-          // ---
-          // PUBLIC METHODS.
-          // ---
-
-
-          /**
-           * Set the tab position and the page title.
-           * @param tabIndex  A tab index.
-           */
-          function setTab(tabIndex) {
-            vm.tabIndex = tabIndex;
-
-            // Set the page title via $window.
-            $window.document.title = vm.tabNames[ tabIndex ];
-
-            // Set the page title.
-            // var appCtrlElement = document.querySelector('html');
-            // var appCtrlScope   = angular.element(appCtrlElement).scope();
-            // appCtrlScope.title = vm.tabNames[ tabIndex ] + " | Masatoshi Nishiguchi";
-          };
-
-
-          /**
-           * Check if a tab is active
-           * @param  tab  A tab index.
-           * @return true if the specify tab index is currently set, else false.
-           */
-          function isSet(tabIndex) {
-
-            return (vm.tabIndex === tabIndex);
-
-          };
-
-        } // end function
-      ] // end controller
     }; // end return
-  }); // end directive
+
+  }; // end tabPanelDirective
+
+
+  tabPanelController.$inject = [
+    "$scope",
+    "$window"
+  ]
+  function tabPanelController( $scope, $window ) {
+
+    var vm    = this;
+    var props = $scope.props = $scope;  // Alias for $scope
+
+    // State
+    vm.tabIndex = 0;  // Tab position initially 0;
+    vm.tabNames = [
+      'About me',
+      'Background',
+      "Projects",
+      'Blog',
+      "Resources",
+      'Contact'
+    ];
+    vm.contentUrls = [
+      "app/components/tabPanel/_about_me.template.html",
+      "app/components/tabPanel/_background.template.html",
+      "app/components/tabPanel/_projects.template.html",
+      "app/components/tabPanel/_blog.template.html",
+      "app/components/tabPanel/_resources.template.html",
+      "app/components/tabPanel/_contact.template.html",
+    ];
+
+    // Expose the public methods.
+    vm.isSet    = isSet;
+    vm.setTab   = setTab;
+    vm.setTitle = setTitle;
+
+
+    // ---
+    // PUBLIC METHODS.
+    // ---
+
+
+    /**
+     * Set the tab position.
+     * @param tabIndex  A tab index.
+     */
+    function setTab(tabIndex) {
+
+      vm.tabIndex = tabIndex;
+
+    };
+
+
+    /**
+     * Check if a tab is active
+     * @param  tab  A tab index.
+     * @return true if the specify tab index is currently set, else false.
+     */
+    function isSet(tabIndex) {
+
+      return (vm.tabIndex === tabIndex);
+
+    };
+
+
+    /**
+     * Set the page title.
+     * @param tabIndex  A tab index.
+     */
+    function setTitle(tabIndex) {
+
+      // Set the page title via $window.
+      $window.document.title = vm.tabNames[ tabIndex ];
+
+    };
+
+  } // end tabPanelController
+
 
 })();
