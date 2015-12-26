ede255cedeae5b80fe8c7931a95d7ce77e0265e3
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2015-12-25 19:23:19 -0500

    Refactor contactForm.directive

--- project/app/components/contactForm/contactForm.directive.js
+++ project/app/components/contactForm/contactForm.directive.js
@@ -4,84 +4,95 @@
 (function() {
 
   // Module declaration.
-  var module = angular.module(
-  "contactFormComponents",
-  []);
+  angular
+    .module( "contactFormComponents", [ ]);
 
 
   // --------------------------------------------------------------------------- //
   // --------------------------------------------------------------------------- //
 
 
-  module.directive(
-  'contactForm',
-  function () {
+  angular
+    .module( "contactFormComponents")
+    .directive( "contactForm", contactFormDirective );
+
+  function contactFormDirective() {
 
     return {
       restrict: "E",
       scope: {},
       templateUrl: "app/components/contactForm/contactForm.template.html",
+      controller: contactFormController,
+      controllerAs: "vm"
+    };
 
-      controllerAs: "vm",
-      controller:
-      [
-        '$scope',
-        function( $scope ) {
-
-          var vm    = this;
-          var props = $scope.props = $scope;  // Alias for $scope
-
-          // State
-          // Current state of the message field
-          vm.message = ""; // Bound to the fields
-          vm.stars   = 0;
-
-          // Expose the public methods.
-          vm.handleMessageSubmit = handleMessageSubmit;
+  }; // end contactFormDirective
 
 
-          // ---
-          // PUBLIC METHODS.
-          // ---
+  contactFormController.$inject = [
+    "$scope"
+  ];
+  function contactFormController( $scope ) {
+
+    var vm    = this;
+    var props = $scope.props = $scope;  // Alias for $scope
+
+    // State
+    // Current state of the message field
+    vm.message = ""; // Bound to the fields
+    vm.stars   = 0;
+
+    // Expose the public methods.
+    vm.handleMessageSubmit = handleMessageSubmit;
 
 
-          // Called when user submits the review
-          function handleMessageSubmit() {
-
-            // Send email.
-            to      = "masatoshi.nishiguchi@udc.edu";
-            subject = "Hello Masa - " + vm.stars + " stars";
-            body    = vm.message;
-            sendEmail( to, "", "", subject, body )
-
-            // Clear the fields
-            vm.message = "";
-            vm.stars   = 0;
-
-            // Reset the form's state
-            $scope.messageForm.$setPristine();
-            $scope.messageForm.$setUntouched();
-          }
+    // ---
+    // PUBLIC METHODS.
+    // ---
 
 
-          // ---
-          // PRIVATE METHODS.
-          // ---
+    /**
+     * Called when user submits the review
+     */
+    function handleMessageSubmit() {
+
+      // Send email.
+      to      = "masatoshi.nishiguchi@udc.edu";
+      subject = "Hello Masa - " + vm.stars + " stars";
+      body    = vm.message;
+      sendEmail( to, "", "", subject, body )
+
+      // Clear the fields
+      vm.message = "";
+      vm.stars   = 0;
+
+      // Reset the form's state
+      $scope.messageForm.$setPristine();
+      $scope.messageForm.$setUntouched();
+
+    }
 
 
-          // I open the default email software with the specified data.
-          function sendEmail(to, cc, bcc, subject, body) {
-            window.location.href = "mailto:" + to
-              + "?cc="  + cc
-              + "&bcc=" + bcc
-              + "&subject=" + escape(subject)
-              + "&body="    + escape(body)
-            ; // end window.location.href
-          }
+    // ---
+    // PRIVATE METHODS.
+    // ---
 
-        } // end function
-      ] // end controller
-    }; // end return
-  }); // end directive
+
+    /**
+     * Open the default email software with the specified data.
+     */
+    function sendEmail(to, cc, bcc, subject, body) {
+
+      window.location.href = "mailto:" + to
+        + "?cc="  + cc
+        + "&bcc=" + bcc
+        + "&subject=" + escape(subject)
+        + "&body="    + escape(body)
+        ; // end window.location.href
+
+    }
+
+  } // end contactFormController
+
 
 })();
